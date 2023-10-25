68371e88f0836aeec219306808ee29d360e0d474
weijiej2964 <73482897+weijiej2964@users.noreply.github.com>
2023-10-25 10:47:24 -0400

    Camera follow

--- Assets/Scenes/Weijie/CameraFollow.cs
+++ Assets/Scenes/Weijie/CameraFollow.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Code : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject target; 
     void Start()
     {
-        print("Hello, code works");
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10); 
     }
 }
